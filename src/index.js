c42fe43af5f26e79a9866977bef3a0874618e2a7
Alondruck <44757262+Alondruck@users.noreply.github.com>
2019-09-23 14:51:03 -0300

    Terminado login, registro, recuperar contra

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import Usuario from './Usuario';
 document.getElementById("btnIngresar").addEventListener('click',function(){
     var user = document.getElementById("exampleInputEmail1").value;
     var password = document.getElementById("exampleInputPassword1").value;
-    console.log("user: " + user);
     Usuario.searchUsername(user,password);
 })
 
--- src/register.js
+++ src/register.js
@@ -1,5 +1,12 @@
 import '../src/style/template.css';
 import { drawFormClient, drawFormDoctor } from './Utilities';
+import Usuario from './Usuario';
+
+document.getElementById("btnIngresar").addEventListener('click',function(){
+    var user = document.getElementById("exampleInputEmail1").value;
+    var password = document.getElementById("exampleInputPassword1").value;
+    Usuario.searchUsername(user,password);
+})
 
 var btnClients = document.getElementById('btnClients');
 var btnDoctors = document.getElementById('btnDoctors');
